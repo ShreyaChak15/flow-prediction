46a2e3e1c86583af6cf914d04c3edf3bfb746f7a
Arun Das <adas21@jhu.edu>
2020-04-20 13:55:16 -0400

    Adding bash script for VM setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# apt-get
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install python3-pip
+# python libraries
+pip3 install sklearn
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install xgboost
+pip3 install tensorflow
+pip3 install keras
+# TODO - add commands to run
