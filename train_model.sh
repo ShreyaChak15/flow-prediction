a2787a89715ef8f6cc2b78d44b884a93e15b2914
Arun Das <adas21@jhu.edu>
2020-05-01 11:01:19 -0400

    Use python3 when training.

--- train_model.sh
+++ train_model.sh
@@ -2,10 +2,10 @@
 echo "Training the models for prediction"
 
 echo -e "\n\n++++++++++++  Feed Forward Neural Network  ++++++++++++"
-python ml/ffnn.py -train
+python3 ml/ffnn.py -train
 
 echo -e "\n\n++++++++++++  Long Short Term Memory  ++++++++++++"
-python ml/lstm.py -train
+python3 ml/lstm.py -train
 
 echo -e "\n\n++++++++++++  XGBoost  ++++++++++++"
-python ml/xgboost_learn.py -train
+python3 ml/xgboost_learn.py -train
