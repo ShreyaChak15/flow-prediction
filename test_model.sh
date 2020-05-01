ed27dee1420d53b11848182637c73dfdf637fc3d
Arun Das <adas21@jhu.edu>
2020-04-30 22:53:00 -0400

    Updated testing script.

--- test_model.sh
+++ test_model.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 echo "Testing the models for prediction"
 
-echo "Feed Forward Neural Network"
+echo -e "\n\n++++++++++++Feed Forward Neural Network++++++++++++"
 python ml/ffnn.py
 
-echo "Long Short Term Memory"
+echo -e "\n\n++++++++++++Long Short Term Memory++++++++++++"
 python ml/lstm.py
 
-echo "XGBoost"
+echo -e "\n\n++++++++++++XGBoost++++++++++++"
 python ml/xgboost_learn.py
